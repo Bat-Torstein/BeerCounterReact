f0b9e07e92a257501dc4ee2dcadbce4a995e0720
Bat-Torstein <torsteib@gmail.com>
2014-12-01 14:47:40 +0100

    added jsx file and build rules

---
+++ jsx/beerCounter.jsx
@@ -0,0 +1,26 @@
+﻿/** @jsx React.DOM */
+
+var BeerCounter = React.createClass({
+    getInitialState: function() {
+        return { counter: 0 }
+    },
+    
+    addBeer: function() {
+        this.setState({counter : this.state.counter + 1});
+    },
+	
+    clearBeer: function() {
+        this.setState({counter: 0});
+    },
+
+    render: function() {
+        var self = this;
+        return <div className='beerDiv'>
+					<img src='img/beer.jpg' onClick={self.addBeer}/>
+					<p>{self.state.counter}</p><br/>
+					<a href='' onClick={self.clearBeer}>Tøm</a>
+				</div>;
+    }
+});
+
+React.render(<BeerCounter/>, document.body);
